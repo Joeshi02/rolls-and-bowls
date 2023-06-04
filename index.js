b32282837d7b3e632ff37baed9365cf44558d172
joacoEscobar <joaco.escobar.h@gmail.com>
2023-06-04 16:59:32 -0300

    corregimos el prompt

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 let entrada = prompt ("ingrese un nombre")
-console.log (typeof entrada)
-let salida = "el nombre ingresado es:" + entrada
-console.log (salida)
+let salida = "el nombre ingresado es: " + entrada
+alert (salida)
